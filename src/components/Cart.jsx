dd126254337b950a294d221de1ebee02b6ebe19f
Cristian Aguirre <63053007+cristianmoto@users.noreply.github.com>
2024-05-01 08:57:32 -0300

    Prueba

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -5,7 +5,7 @@ const Cart = () => {
       <div className="flex pr-5 pt-2">
         <button type="button" className="btn btn-primary relative">
           <div className="flex items-center">
-            <img className="w-12" src={cart} alt="carrito" />
+            <img className="w-12" src={cart} alt="Carrito" />
             <span className="absolute top-0 right-0 bg-red-500 text-white font-semibold px-2 py-1 rounded-full">5</span>
           </div>
         </button>
